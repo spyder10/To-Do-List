d152f4a7020cf1491c136a09e4e26be07f9fa988
vibhor <hi.vibhor10@gmail.com>
2021-03-29 20:38:38 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require ("body-parser");
+
+const app = express();
+app.use(express.urlencoded({extended:true}));
+app.set('view engine','ejs');
+app.use(express.static("public"));
+let day="";
+let items =[];
+app.get("/",function(req,res){
+    let today= new Date();
+    let options={
+        weekday : "long",
+        day : "numeric",
+        month :"long"
+    };
+    day=today.toLocaleDateString("en-US",options);
+    res.render("list",{day:day,items:items});
+});
+app.post("/",function(req,res){
+        items.push(req.body.newItem);
+    res.redirect("/");
+});
+app.listen(3000,function(){
+    console.log("Server is running on port 3000");
+});
